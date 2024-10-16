3d13921c81c081e05afdd9c05f8d350d09c91985
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-10-16 16:34:35 +0530

    Setup routes for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { Home, BuyCredit, Result } from "./pages/page_index";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<BuyCredit />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/page_index.js
@@ -0,0 +1,9 @@
+import Home from "../pages/Home"
+import Result from "../pages/Result"
+import BuyCredit from "../pages/BuyCredit" 
+
+export {
+    Home,
+    Result,
+    BuyCredit
+}
