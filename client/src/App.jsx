c8fb5401b7949ac420c31402e078b566ca8cb329
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-10-16 18:43:49 +0530

    Create Navigation bar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import { Home, BuyCredit, Result } from "./pages/page_index";
+import { Navbar } from './components/component_index';
 
 function App() {
 
   return (
-    <>
+    <div className='min-h-screen bg-slate-50'>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/buy" element={<BuyCredit />} />
         <Route path="/result" element={<Result />} />
       </Routes>
-    </>
+    </div>
   )
 }
 
