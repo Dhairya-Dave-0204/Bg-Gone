17bec161a0c86435aa6a1972ff876b0236b66532
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-10-17 09:56:33 +0530

    Create slider for preview of result

--- client/src/components/component_index.js
+++ client/src/components/component_index.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
 import Header from './Header';
 import StepCard from './StepCard';
+import BgSlider from './BgSlider'
 
 export {
     Navbar,
     Header,
-    StepCard
+    StepCard,
+    BgSlider
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Header, StepCard } from '../components/component_index';
+import { Header, StepCard, BgSlider } from '../components/component_index';
 
 function Home() {
   return (
     <div>
       <Header/>
       <StepCard/>
+      <BgSlider />
     </div>
   )
 }
